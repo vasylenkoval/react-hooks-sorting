4f6287c069b305a144613ab55755043364012519
Valentyn Vasylenko <vasylenko.valentyn@gmail.com>
2019-05-30 02:41:24 -0400

    Updates

---
+++ precache-manifest.1378f7d55012487562b700a6e5251df5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46afee7345a4898562457222cafb8fa2",
+    "url": "/react-hooks-sorting/index.html"
+  },
+  {
+    "revision": "ac0ade9b9933870081ad",
+    "url": "/react-hooks-sorting/static/js/2.5e256f2b.chunk.js"
+  },
+  {
+    "revision": "266e67bbb1f4d64dc362",
+    "url": "/react-hooks-sorting/static/js/main.1fc8514e.chunk.js"
+  },
+  {
+    "revision": "5494e8a5eb87ab25e4fe",
+    "url": "/react-hooks-sorting/static/js/runtime~main.e4d41628.js"
+  },
+  {
+    "revision": "a356a8420508c6d5552db614a0451be8",
+    "url": "/react-hooks-sorting/static/media/beep.a356a842.wav"
+  }
+]);
